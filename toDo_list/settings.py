8c2b8f8f4fa2b80f318c23862c5f7b3d04453298
zeedtanue <zeedtanue@gmail.com>
2019-01-17 15:43:47 +0800

    Update settings.py

--- toDo_list/settings.py
+++ toDo_list/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'toDo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Todo',
-        'User': 'Todouser',
-        'Password': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -123,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
